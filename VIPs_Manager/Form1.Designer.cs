b978760599b3a1ce17b25502bea308dc6c0f713e
Mike-Brown8 <nohuitou@outlook.com>
2021-10-13 12:17:08 +0800

    开发版更新

---
+++ VIPs_Manager/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace VIPs_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButtonAdd = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonDel = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonOut = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonIn = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripDropDownButtonMode = new System.Windows.Forms.ToolStripDropDownButton();
+            this.SetModeNormal = new System.Windows.Forms.ToolStripMenuItem();
+            this.SetModePro = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.ToolStripButtonAbout = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripSeparator1,
+            this.toolStripButtonAdd,
+            this.toolStripButtonDel,
+            this.toolStripButtonOut,
+            this.toolStripButtonIn,
+            this.toolStripSeparator2,
+            this.toolStripDropDownButtonMode,
+            this.toolStripSeparator3,
+            this.ToolStripButtonAbout,
+            this.toolStripLabel2});
+            resources.ApplyResources(this.toolStrip1, "toolStrip1");
+            this.toolStrip1.Name = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripLabel1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            resources.ApplyResources(this.toolStripLabel1, "toolStripLabel1");
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            resources.ApplyResources(this.toolStripSeparator1, "toolStripSeparator1");
+            // 
+            // toolStripButtonAdd
+            // 
+            this.toolStripButtonAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            resources.ApplyResources(this.toolStripButtonAdd, "toolStripButtonAdd");
+            this.toolStripButtonAdd.Name = "toolStripButtonAdd";
+            // 
+            // toolStripButtonDel
+            // 
+            this.toolStripButtonDel.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            resources.ApplyResources(this.toolStripButtonDel, "toolStripButtonDel");
+            this.toolStripButtonDel.Name = "toolStripButtonDel";
+            // 
+            // toolStripButtonOut
+            // 
+            this.toolStripButtonOut.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            resources.ApplyResources(this.toolStripButtonOut, "toolStripButtonOut");
+            this.toolStripButtonOut.Name = "toolStripButtonOut";
+            // 
+            // toolStripButtonIn
+            // 
+            this.toolStripButtonIn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            resources.ApplyResources(this.toolStripButtonIn, "toolStripButtonIn");
+            this.toolStripButtonIn.Name = "toolStripButtonIn";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            resources.ApplyResources(this.toolStripSeparator2, "toolStripSeparator2");
+            // 
+            // toolStripDropDownButtonMode
+            // 
+            this.toolStripDropDownButtonMode.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButtonMode.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SetModeNormal,
+            this.SetModePro});
+            resources.ApplyResources(this.toolStripDropDownButtonMode, "toolStripDropDownButtonMode");
+            this.toolStripDropDownButtonMode.Name = "toolStripDropDownButtonMode";
+            // 
+            // SetModeNormal
+            // 
+            this.SetModeNormal.Checked = true;
+            this.SetModeNormal.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.SetModeNormal.Name = "SetModeNormal";
+            resources.ApplyResources(this.SetModeNormal, "SetModeNormal");
+            this.SetModeNormal.Click += new System.EventHandler(this.SetModeNormal_Click);
+            // 
+            // SetModePro
+            // 
+            this.SetModePro.Name = "SetModePro";
+            resources.ApplyResources(this.SetModePro, "SetModePro");
+            this.SetModePro.Click += new System.EventHandler(this.SetModePro_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            resources.ApplyResources(this.toolStripSeparator3, "toolStripSeparator3");
+            // 
+            // ToolStripButtonAbout
+            // 
+            this.ToolStripButtonAbout.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            resources.ApplyResources(this.ToolStripButtonAbout, "ToolStripButtonAbout");
+            this.ToolStripButtonAbout.Name = "ToolStripButtonAbout";
+            this.ToolStripButtonAbout.Click += new System.EventHandler(this.ToolBarAboutButton_Click);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.ForeColor = System.Drawing.Color.Red;
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            resources.ApplyResources(this.toolStripLabel2, "toolStripLabel2");
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton ToolStripButtonAbout;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonAdd;
+        private System.Windows.Forms.ToolStripButton toolStripButtonDel;
+        private System.Windows.Forms.ToolStripButton toolStripButtonOut;
+        private System.Windows.Forms.ToolStripButton toolStripButtonIn;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButtonMode;
+        private System.Windows.Forms.ToolStripMenuItem SetModeNormal;
+        private System.Windows.Forms.ToolStripMenuItem SetModePro;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+    }
+}
+
---
+++ VIPs_Manager/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VIPs_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolBarAboutButton_Click(object sender, EventArgs e) //关于按钮点击函数
+        {
+            AboutBox1 AboutBox1 = new AboutBox1();
+            AboutBox1.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) //窗口载入函数
+        {
+            
+        }
+
+        private void SetModeNormal_Click(object sender, EventArgs e) //标准模式点击函数
+        {
+
+        }
+
+        private void SetModePro_Click(object sender, EventArgs e) //专家模式点击函数
+        {
+
+        }
+    }
+}
